c58a83d36104e352f53e900820332e8251c813ee
Zphinx.GZ <zeeyou.ou@gmail.com>
2024-10-04 09:46:39 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import { HashRouter,Routes,Route } from 'react-router-dom'
 
 import Home from './page/home/Home'
 import Todo from './page/Todo/Todo'
-
+import Layout from './Layout/Layouts/Layout'
 
 
 import './App.css'
 
 function App() {
   return (
-    <>
-     <h1>MULTIPAGE</h1>
-     <Home />
-     <Todo />
-    </>
+    <div>
+     <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+     </HashRouter>
+    </div>
   )
 }
 
