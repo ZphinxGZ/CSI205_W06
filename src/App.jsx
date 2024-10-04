66c781444521147574b6d6bbe5131ef22b885f4c
Zphinx.GZ <zeeyou.ou@gmail.com>
2024-10-04 09:39:54 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+
+import Home from './page/home/Home'
+import Todo from './page/Todo/Todo'
+
+
+
 import './App.css'
 
 function App() {
   return (
     <>
      <h1>MULTIPAGE</h1>
+     <Home />
+     <Todo />
     </>
   )
 }
---
+++ src/page/Todo/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Todo.css'
+
+function Todo() {
+  return (
+    <div>
+      <h1>TODO</h1>
+    </div>
+  )
+}
+
+export default Todo
